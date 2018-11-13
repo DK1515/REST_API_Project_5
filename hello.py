f3da956515db38b94ade6c95c63a8ea3700ce5bd
DK1515 <dk15@tamu.edu>
2018-11-12 23:18:27 -0600

    Formatting and semi-working validation

--- hello.py
+++ hello.py
@@ -12,63 +12,80 @@ def hello_world():
 
 @app.route('/md5/<strung>')
 def md5conv(strung):
-     return hashlib.md5(strung).hexdigest()
+     return json.dumps(hashlib.md5(strung).hexdigest())
 
 @app.route("/factorial/<int:n>/")
 def factorial(n):
+    input = n
     if isinstance(n, int):
         if n > 0:
             num = 1
             while n >= 1:
                 num = num * n
                 n = n - 1
-            return json.dumps(num)
+            final = ('input:',input,'output:',num)
+	    return json.dumps(final)
         else:
             return json.dumps('ERROR: That is not a number or the number is negative.')
     else:
         return json.dumps('ERROR: This is not a number.')
 
 
+
 @app.route("/fibonacci/<int:param_fi>/")
 def getFib(param_fi):
-    i = 0
-    j = 1
-    sequence = []
-    current_operation = 0
-    index = 0
-    while True:
-        sequence.append(i)
-        current_operation = i + j
-        i = j
-        j = current_operation
-        if i > param_fi:
-            return json.dumps(sequence)
-        else:
-            index += 1
-    return json.dumps(sequence)
+   if param_fi >= 0: 
+	    input = param_fi
+	    i = 0
+	    j = 1
+	    sequence = []
+	    current_operation = 0
+	    index = 0
+	    while True:
+		sequence.append(i)
+		current_operation = i + j
+		i = j
+		j = current_operation
+		if i > param_fi:
+		    final = ('input:', input, 'output:', sequence)
+		    return json.dumps(final)
+		else:
+		    index += 1
+	    return json.dumps(sequence)
+   else:
+	return json.dumps('ERROR: You input is invalid')
 
 
 @app.route("/is-prime/<int:fact>/")
 def getPrime(fact):
+   if fact >= 0:
+	input = fact
 	for n in range(2, fact - 1):  # checking if fact is composite
 	    if (fact % n) == 0:
-	        return json.dumps('Composite')
-	if(fact == 0 or fact == 1):  # checking if 1 is prime and not sure of the status for 0
-		 return json.dumps('Prime')
+		final = ('input:', input, 'output: Composite')
+	        return json.dumps(final)
+	if(fact == 0 or fact == 1):  # checking if 1 is prime and not sure of the status for 0	
+		final = ('input:', input, 'output: Prime') 
+		return json.dumps(final)
 	else:  # every number that is not 1 and not composite is prime
-	    return json.dumps('Prime')
+		final = ('input:', input, 'output: Prime')
+		return json.dumps(final)
+   else:
+	return json.dumps('ERROR: Your input is invalid')
 
 
 
 @app.route("/slack-alert/<string:name>/")
 def getSlack(name):
+    input = name
     web_hook_url = 'https://hooks.slack.com/services/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'
     slack_msg = {'text': name}
     if requests.post(web_hook_url, data=json.dumps(slack_msg)):
-        print(name)
-        return json.dumps(True)
+        final = ('input:', input, 'output:', True)
+        return json.dumps(final)
     else:
-        return json.dumps(False)
+	final = ('input:', input, 'output', False)
+        return json.dumps(final)
 
 if __name__ == "__main__":
     app.run()
