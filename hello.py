7ab9b3ed6efe44392927389b722f1d1b00f17bb9
D <dk15@tamu.edu>
2018-11-20 11:33:27 -0600

    refix

--- hello.py
+++ hello.py
@@ -12,9 +12,10 @@ def hello_world():
 
 @app.route('/md5/<strung>')
 def md5conv(strung):
-	input=strung
-	final=(hashlib.md5(strung).hexdigest())
-	return jsonify(strung,final)
+    input = strung
+    final = (hashlib.md5(strung .encode('utf-8')).hexdigest())
+    return jsonify(strung, final)
+
 
 @app.route("/factorial/<int:n>")
 def factorial(n):
@@ -25,62 +26,60 @@ def factorial(n):
             while n >= 1:
                 num = num * n
                 n = n - 1
-	    return jsonify(input,num)
+            return jsonify(input, num)
         else:
             return jsonify('ERROR: That is not a number or the number is negative.')
     else:
         return jsonify('ERROR: This is not a number.')
 
 
-
 @app.route("/fibonacci/<int:param_fi>")
 def getFib(param_fi):
-   if param_fi >= 0:
-	    input = param_fi
-	    i = 0
-	    j = 1
-	    sequence = []
-	    current_operation = 0
-	    index = 0
-	    while True:
-		sequence.append(i)
-		current_operation = i + j
-		i = j
-		j = current_operation
-		if i > param_fi:
-		    return jsonify(input,sequence)
-		else:
-		    index += 1
-	    return jsonify(input,sequence)
-   else:
-	return jsonify('ERROR: You input is invalid')
+    if param_fi >= 0:
+        input = param_fi
+        i = 0
+        j = 1
+        sequence = []
+        current_operation = 0
+        index = 0
+        while True:
+            sequence.append(i)
+            current_operation = i + j
+            i = j
+            j = current_operation
+            if i > param_fi:
+                return jsonify(input, sequence)
+            else:
+                index += 1
+        return jsonify(input, sequence)
+    else:
+        return jsonify('ERROR: You input is invalid')
 
 
 @app.route("/is-prime/<int:fact>")
 def getPrime(fact):
-   if fact >= 0:
-	input = fact
-	for n in range(2, fact - 1):  # checking if fact is composite
-	    if (fact % n) == 0:
-	        return jsonify(input,False)
-	if(fact == 0 or fact == 1):  # checking if 1 is prime and not sure of the status for 0
-		return jsonify(input,False)
-	else:  # every number that is not 1 and not composite is prime
-		return jsonify(input,True)
-   else:
-	return jsonify('ERROR: Your input is invalid')
-
+    if fact >= 0:
+        input = fact
+        for n in range(2, fact - 1):  # checking if fact is composite
+            if (fact % n) == 0:
+                return jsonify(input, False)
+        if(fact == 0 or fact == 1):  # checking if 1 is prime and not sure of the status for 0
+            return jsonify(input, False)
+        else:  # every number that is not 1 and not composite is prime
+            return jsonify(input, True)
+    else:
+        return jsonify('ERROR: Your input is invalid')
 
 
 @app.route("/slack-alert/<string:name>")
 def getSlack(name):
     input = name
-    web_hook_url = 'https://hooks.slack.com/services/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'
+    web_hook_url = 'https://hooks.slack.com/services/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'
     slack_msg = {'text': name}
     if requests.post(web_hook_url, data=json.dumps(slack_msg)):
-        return jsonify(input,True)
+        return jsonify(input, True)
     else:
-        return jsonify(input,False)
+        return jsonify(input, False)
 
 if __name__ == "__main__":
     app.run()
