f9662c8d31f738938947e4298661e45ad4b3525b
gsifuentez8867 <gsifuentez8867@tamu.edu>
2018-12-03 17:53:23 -0600

    Update test.py

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ for uri, test_result in tests.items():
     print " * ", uri, "... ",
 	if(uri=='/kv-record/name key=test value= val')
 		resp= requests.get('http POST localhost:5000'+uri)
-		else if(uri=='/kv-record/name key=bad value=val')
+		else if(uri=='/kv-record/name key=test value=newval')
 				resp = requests.get('http PUT localhost:5000'+uri)
 			else if (uri == '/kv-record/name key=bad value=val')
 					resp = requests.get('http PUT localhost:5000'+uri)
